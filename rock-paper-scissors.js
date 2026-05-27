c3b17bab23c67e2ee320df3103aa2f641d10b9a4
Jerish Immanuel J <jerishjimmanuel114@gmail.com>
2026-05-27 18:42:25 +0530

    Implement autoplay feature and button listeners
    Added autoplay feature to play the game automatically every second. Included event listeners for buttons and keyboard shortcuts to trigger game actions.

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -6,6 +6,64 @@ let score = JSON.parse(localStorage.getItem('score')) || {
 
 updateScoreElement();
 
+let isAutoPlaying = false;
+let intervalId;
+
+function autoPlay() {
+    if (!isAutoPlaying) {
+        intervalId = setInterval(() => {
+            const playerMove = pickComputerMove();
+            playGame(playerMove);
+        }, 1000);
+        isAutoPlaying = true;
+    } else {
+        clearInterval(intervalId);
+        isAutoPlaying = false;
+    }
+}
+
+document.querySelector('.js-rock-button')
+    .addEventListener('click', () => {
+        playGame('rock');
+    });
+
+document.querySelector('.js-paper-button')
+    .addEventListener('click', () => {
+        playGame('paper');
+    });
+
+document.querySelector('.js-scissors-button')
+    .addEventListener('click', () => {
+        playGame('scissors');
+    });
+
+document.querySelector('.js-reset-score-button')
+    .addEventListener('click', resetScore);
+
+document.querySelector('.js-auto-play')
+    .addEventListener('click', autoPlay);
+
+document.body.addEventListener('keydown', (event) => {
+    if(event.key === 'r') {
+        playGame('rock');
+    } else if (event.key === 'p') {
+        playGame('paper');
+    } else if (event.key === 's') {
+        playGame('scissors');
+    } 
+});
+
+function resetScore() {
+    score = {
+        wins: 0,
+        losses: 0,
+        ties: 0
+    };
+
+    localStorage.setItem('score', JSON.stringify(score));
+
+    updateScoreElement();
+}
 
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
